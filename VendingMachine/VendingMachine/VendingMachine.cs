bfb541decd0e42e6d4d911e040bd5db8a6644c25
ARoessner <And13@comcast.net>
2018-05-30 12:26:51 -0400

    Initial Commit of Vending Machine and Tests

---
+++ VendingMachine/VendingMachine/VendingMachine.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class main
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+    class VendingMachine
+    {
+        //Product Names
+        string[] products;
+        //Product Amounts
+        int[] numProducts;
+        //Price Products
+        double[] price;
+        //Current Balance
+        double changeInserted = 0;
+
+        public VendingMachine()
+        {
+            this.products = new string[] {"cola","chips","candy" };
+            this.numProducts = new int[] { 3, 3, 3 };
+            this.price = new double[] { 1, .5, .65 };
+        }
+        public VendingMachine(string[] products,int[] numProducts, double[] price)
+        {
+            this.products = products;
+            this.numProducts = numProducts;
+            this.price = price;
+        }
+        public string InsertCoin(string coin)
+        {
+            if ((coin == "nickel") || (coin == "Nickel"))
+            {
+                changeInserted += .05;
+            }
+            else if ((coin == "dime") || (coin == "Dime"))
+            {
+                changeInserted += .10;
+            }
+            else if ((coin == "quarter") || (coin == "Quarter"))
+            {
+                changeInserted += .25;
+            }
+            return changeInserted.ToString("C");
+        }
+
+        public string Dispense(int product)
+        {
+            if(numProducts[product]>0)
+            {
+                if (changeInserted >= price[product])
+                {
+                    numProducts[product]--;
+                    changeInserted = 0;
+                    return "Thank You!!!";
+                }
+                else
+                {
+                    return "PRICE: " + price[product].ToString("C");
+                }
+            }
+            else
+            {
+                return "SOLD OUT";
+            }
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/VendingMachineTest.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    [TestFixture]
+    class VendingMachineTest
+    {
+        [TestCase("penny", "$0.00")]
+        [TestCase("nickel", "$0.05")]
+        [TestCase("dime", "$0.10")]
+        [TestCase("quarter", "$0.25")]
+        [TestCase("Penny", "$0.00")]
+        [TestCase("Nickel", "$0.05")]
+        [TestCase("Dime", "$0.10")]
+        [TestCase("Quarter", "$0.25")]
+        [TestCase("asdfjk", "$0.00")]
+        public void InsertCoin(string coin, string expected)
+        {
+            VendingMachine vm = new VendingMachine();
+            Assert.AreEqual(expected, vm.InsertCoin(coin));
+        }
+        [TestCase(0, "PRICE: $1.00")]
+        [TestCase(1, "PRICE: $0.50")]
+        [TestCase(2, "PRICE: $0.65")]
+        public void NotEnoughMoneyDispense(int p, string expected)
+        {
+            VendingMachine vm = new VendingMachine();
+            Assert.AreEqual(expected, vm.Dispense(p));
+        }
+        [TestCase(0, "SOLD OUT")]
+        [TestCase(1, "SOLD OUT")]
+        [TestCase(2, "SOLD OUT")]
+        public void NoProductsDispense(int p, string expected)
+        {
+            VendingMachine vm = new VendingMachine();
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.Dispense(p);
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.Dispense(p);
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.Dispense(p);
+            Assert.AreEqual(expected, vm.Dispense(p));
+        }
+        [TestCase(0, "Thank You!!!")]
+        [TestCase(1, "Thank You!!!")]
+        [TestCase(2, "Thank You!!!")]
+        public void ValidDispense(int p, string expected)
+        {
+            VendingMachine vm = new VendingMachine();
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            vm.InsertCoin("Quarter");
+            Assert.AreEqual(expected, vm.Dispense(p));
+        }
+    }
+}
